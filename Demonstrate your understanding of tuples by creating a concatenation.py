3f17a1ce6d8e57f77ff1e95b0372f7d58685f673
Anacondarch <83915776+Anacondarch@users.noreply.github.com>
2021-06-09 13:33:56 +0100

    Add files via upload

---
+++ Demonstrate your understanding of tuples by creating a concatenation.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Demonstrate your understanding of tuples by creating a concatenation
+
+# In[1]:
+
+
+str1= "Welcome"
+str2= "Home"
+print("string 1:" , str1 )
+print("string 2:" , str2)
+str=str1+str2
+print("Concatenated two different strings:", str)
+
+
+# In[ ]:
+
+
+
+
