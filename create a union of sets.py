d62a2bab0cad0eb57d57240c97b0ce55bfc0a1ad
Anacondarch <83915776+Anacondarch@users.noreply.github.com>
2021-06-09 14:16:13 +0100

    Add files via upload

---
+++ create a union of sets.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# `Using the '|' operator, create a union of sets`
+
+# In[ ]:
+
+
+# sets are define
+A = {0, 4, 5, 6, 7, 8};
+B = {0, 3, 4, 6, 8, 9};
+# union
+print("Union :", A | B)
+
