76e9a1ad3e416cccb6e316f73a28aa7c51d12bec
Anacondarch <83915776+Anacondarch@users.noreply.github.com>
2021-07-01 18:53:39 +0100

    Add files via upload

---
+++ Probability Assignment.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# A pack contains 4 blue, 2 red and 3 black pens. If 2 pens are drawn at random from the pack, NOT replaced and then another pen is drawn. What is the probability of drawing 2 blue pens and 1 black pen?
+
+# Firstly, we get the sum of all the pen which is equal to 9. first we find the probability of drawing a black pen which is
+
+# In[6]:
+
+
+#Probability of drawing two blue pen and 1 black pen
+(4/9)*(4/9)*(3/9)
+
+
+# A box contains 4 mangoes and 4 oranges. Dan eats 3 of them one after another. What is the probability of sequentially choosing 2 mangoes and 1 orange
+
+# In[7]:
+
+
+#the probalility of getting 2 mangoes and 1 orange.
+(4/8)*(4/8)*(4/8)
+
+
+# A bag contains blue and red balls. Two balls are drawn randomly without replacement. The probability of selecting a blue and then a red ball is 0.2. The probability of selecting a blue ball in the first draw is 0.5. What is the probability of drawing a red ball, given that the first ball drawn was blue
+
+# Number of balls drawn without replacement=2
+# The Probability of selecting a blue and then a red ball is 0.2
+# The Probability of selecting a blue ball in the first draw is 0.5
+# 
+
+# In[18]:
+
+
+#Let events B and R denotes the blue and red ball respectively
+0.2/0.5
+
+
+# In[ ]:
+
+
+
+
